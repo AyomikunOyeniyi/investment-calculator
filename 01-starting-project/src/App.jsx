e027b4d9e5518eb64d9118fa469eda3683d6423b
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-07 04:06:07 -0400

    Import results component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,9 +1,11 @@
-import Input from "./components/Input"
+import Input from "./components/Input";
+import Results from "./components/Results";
 
 function App() {
   return (
     <div>
       <Input></Input>
+      <Results></Results>
     </div>
   )
 }
