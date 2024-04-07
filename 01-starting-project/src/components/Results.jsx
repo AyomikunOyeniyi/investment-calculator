3d65d7e1582ddc9945a7fd08804653e59de98521
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-07 04:05:20 -0400

    Return table

---
+++ 01-starting-project/src/components/Results.jsx
@@ -0,0 +1,17 @@
+export default function Results() {
+    return(
+        <section>
+            <table id="result">
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Investment Value</th>
+                        <th>Interest (Year)</th>
+                        <th>Total Interest</th>
+                        <th>Invested Capital</th>
+                    </tr>
+                </thead>
+            </table>
+        </section>
+    )
+}
