13e40b574e7b4dee2f38ebf4a06561397efdd5dd
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-07 23:26:05 -0400

    Import functions from investment.js

--- 01-starting-project/src/components/Results.jsx
+++ 01-starting-project/src/components/Results.jsx
@@ -1,4 +1,10 @@
-export default function Results() {
+import { calculateInvestmentResults } from "../util/investment";
+import { formatter } from "../util/investment";
+
+export default function Results({tableObject}) {
+    const investmentResults = calculateInvestmentResults(tableObject);
+    const initialInvestment = investmentResults[0].valueEndOfYear - investmentResults[0].interest - investmentResults[0].annualInvestment;
+    
     return(
         <section>
             <table id="result">
@@ -11,6 +17,19 @@ export default function Results() {
                         <th>Invested Capital</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {investmentResults.map((result) => {
+                        return (
+                            <tr key={result.year}>
+                                <td>{result.year}</td>
+                                <td>{formatter.format(result.valueEndOfYear)}</td>
+                                <td>{formatter.format(result.interest)}</td>
+                                <td>{formatter.format(result.valueEndOfYear - result.annualInvestment * result.year - initialInvestment)}</td>
+                                <td>{formatter.format(result.valueEndOfYear - result.interest)}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
             </table>
         </section>
     )
