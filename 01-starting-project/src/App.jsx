69467bb1150a6788bc3cc47ab50ab9ae910e0588
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-07 23:25:10 -0400

    Complete component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,29 @@
 import Input from "./components/Input";
 import Results from "./components/Results";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment : 40000,
+    annualInvestment : 1000,
+    expectedReturn : 5,
+    duration : 10
+  });
+
+  function handleUserInput(fieldIdentifier, newValue) {
+    setUserInput((formerValue) => {
+        return {
+          ...formerValue,
+          [fieldIdentifier]: +newValue,
+        }
+    });
+  }
+
+  
   return (
     <div>
-      <Input></Input>
-      <Results></Results>
+      <Input inputObject={userInput} changeHandler={handleUserInput}></Input>
+      <Results tableObject={{...userInput}}></Results>
     </div>
   )
 }
